908fd8774bb02c8737765b5df45c69da2650aa91
Diango <diangogavidia@gmail.com>
2018-06-19 08:47:59 -0400

    Pagina de inicio con sesion iniciada HomePage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
-import { GenderPage } from '../pages/gender/gender';
 import { AuthProvider } from '../providers/auth/auth';
 import { HomePage } from '../pages/home/home';
 
