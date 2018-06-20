aab85a116be42d0578704d6d2a5e2d1ea511e40e
Diango <diangogavidia@gmail.com>
2018-06-20 00:26:38 -0400

    Se agregó la barra de progreso circular sustituyendo el texto tanto para el pulso como para el oxigeno

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component,ViewChild } from '@angular/core';
-import { NavController,AlertController  } from 'ionic-angular';
+import { NavController,AlertController,IonicPage  } from 'ionic-angular';
 import { Chart } from 'chart.js';
 import { ViewPage } from '../view/view';
 import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
@@ -8,6 +8,7 @@ import firebase from 'firebase';
 import { AuthProvider } from '../../providers/auth/auth';
 import { FirebaseDbProvider } from '../../providers/firebase-db/firebase-db';
 import 'rxjs/add/observable/interval'
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -17,6 +18,8 @@ export class HomePage {
 
   @ViewChild('lineCanvas') lineCanvas;
   @ViewChild('lineCanvas2') lineCanvas2;
+  maxOxygen = 98;
+  maxPulse = 98;
   lineChart: any;
   items;
   itemsPulse;
@@ -78,7 +81,8 @@ export class HomePage {
 
 
   }
-
+//********************************************************************************
+//GRÁFICA 2
        chartDataPulse(values,labels){
         this.lineChart = new Chart(this.lineCanvas2.nativeElement, {      
             type: 'line',
@@ -89,17 +93,17 @@ export class HomePage {
                         label: "Realtime Data",
                         fill: false,
                         lineTension: 0.1,
-                        backgroundColor: "rgba(75,192,192,0.4)",
-                        borderColor: "rgba(75,192,192,1)",
+                        backgroundColor: "rgba(214,10,10,0.4)",
+                        borderColor: "rgba(214, 10, 10,1)",
                         borderCapStyle: 'butt',
                         borderDash: [],
                         borderDashOffset: 0.0,
                         borderJoinStyle: 'miter',
-                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBorderColor: "rgba(214,10,10,1)",
                         pointBackgroundColor: "#fff",
                         pointBorderWidth: 1,
                         pointHoverRadius: 5,
-                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBackgroundColor: "rgba(214,10,10,1)",
                         pointHoverBorderColor: "rgba(220,220,220,1)",
                         pointHoverBorderWidth: 2,
                         pointRadius: 1,
@@ -114,6 +118,8 @@ export class HomePage {
 
   
 //==================================================================================================
+ //GRAFICA 1
+
       chartData(values,labels){
         this.lineChart = new Chart(this.lineCanvas.nativeElement, {      
             type: 'line',
@@ -124,17 +130,17 @@ export class HomePage {
                         label: "Realtime Data",
                         fill: false,
                         lineTension: 0.1,
-                        backgroundColor: "rgba(75,192,192,0.4)",
-                        borderColor: "rgba(75,192,192,1)",
+                        backgroundColor: "rgba(32, 186, 252,0.4)",
+                        borderColor: "rgba(32, 186, 252,1)",
                         borderCapStyle: 'butt',
                         borderDash: [],
                         borderDashOffset: 0.0,
                         borderJoinStyle: 'miter',
-                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBorderColor: "rgba(32, 186, 252,1)",
                         pointBackgroundColor: "#fff",
                         pointBorderWidth: 1,
                         pointHoverRadius: 5,
-                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBackgroundColor: "rgba(32, 186, 252,1)",
                         pointHoverBorderColor: "rgba(220,220,220,1)",
                         pointHoverBorderWidth: 2,
                         pointRadius: 1,
@@ -162,16 +168,16 @@ export class HomePage {
         this.flag4 = false;
       }
       hidden4(){
-        this.flag3 = true;
-        this.flag4 = false;
+        this.flag3 = false;
+        this.flag4 = true;
       }
 
 
       startTimer(){
 
         this.timerVar = Observable.interval(5000).subscribe(x => {
-                  this.firebasedatabase.saveOxygenData(Math.round(Math.random()*(98-75)+75))
-                  this.firebasedatabase.savePulseData(Math.round(Math.random()*(98-75)+75))
+                  this.firebasedatabase.saveOxygenData(Math.round(Math.random()*(this.maxOxygen-75)+75))
+                  this.firebasedatabase.savePulseData(Math.round(Math.random()*(this.maxPulse-75)+75))
         })
 
       }
