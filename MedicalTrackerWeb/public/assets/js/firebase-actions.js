149e44387503eca9c7d2082af1454a75494434be
Diango <diangogavidia@gmail.com>
2018-07-03 23:00:07 -0400

    En este archivo irán todas las funciones relacionadas al manejo de la base de datos que serán migradas desde index.js progresivamente

---
+++ MedicalTrackerWeb/public/assets/js/firebase-actions.js
@@ -0,0 +1,14 @@
+//************************************************************************************************************************** */
+//***********************************************Firebase Operations******************************************************* */
+//************************************************************************************************************************ */
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC3OSEMlsGgp0Y7kVDNNO1cpgTshj95m2c",
+    authDomain: "hospital-645ee.firebaseapp.com",
+    databaseURL: "https://hospital-645ee.firebaseio.com",
+    projectId: "hospital-645ee",
+    storageBucket: "hospital-645ee.appspot.com",
+    messagingSenderId: "761286977231"
+  };
+  firebase.initializeApp(config);
