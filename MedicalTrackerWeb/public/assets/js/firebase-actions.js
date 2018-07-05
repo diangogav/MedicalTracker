d70985e7ce0438d4ae98b2a6a376d0c585f3697f
Diango <diangogavidia@gmail.com>
2018-07-05 17:38:50 -0400

    Funciones referentes a peticiones a la base de datos firebase

--- MedicalTrackerWeb/public/assets/js/firebase-actions.js
+++ MedicalTrackerWeb/public/assets/js/firebase-actions.js
@@ -12,3 +12,144 @@ var config = {
     messagingSenderId: "761286977231"
   };
   firebase.initializeApp(config);
+
+  //*******************************************************/
+
+  function firebaseInfiniteScroll(route,pointer,callback){
+
+    var arrayOfKeys;
+
+    if(pointer == undefined){
+
+
+    }else if(!pointer){
+
+        firebase.database().ref(route)
+        .orderByKey()
+        .limitToLast(20)
+        .once('value', function (dataSnapshot) {
+    
+            arrayOfKeys = Object.keys(dataSnapshot.val())
+            .sort()
+            .reverse();
+    
+            var results = arrayOfKeys
+            .map((key) => dataSnapshot.val()[key]);
+
+            pointer = arrayOfKeys[arrayOfKeys.length-1];
+            
+            callback({
+                "pointer":arrayOfKeys[arrayOfKeys.length-1], 
+                "keys": arrayOfKeys,
+                "values":results
+            });
+    
+        }, function (errorObject) {
+    
+            console.log("The read failed: " + errorObject.code);
+    
+        });
+
+    }else{
+
+
+        firebase.database().ref(route)
+        .orderByKey()
+        .endAt(pointer)
+        .limitToLast(20)
+        .once('value', function (dataSnapshot) {
+    
+            arrayOfKeys = Object.keys(dataSnapshot.val())
+            .sort()
+            .reverse()
+            .slice(1);
+
+            var results = arrayOfKeys
+            .map((key) => dataSnapshot.val()[key]);
+
+            pointer = arrayOfKeys[arrayOfKeys.length-1];
+    
+            callback({
+                "pointer":arrayOfKeys[arrayOfKeys.length-1], 
+                "keys": arrayOfKeys,
+                "values":results
+            });
+    
+        }, function (errorObject) {
+    
+            console.log("The read failed: " + errorObject.code);
+    
+        });
+    }
+  }
+  //*******************************************************/
+
+  function firebaseGetKeys(route,callback){
+
+    firebase.database().ref(route)
+    .limitToLast(10)
+    .once("value",function(snapshot){
+
+    var keys;
+
+    snapshot.forEach((childSnapshot) => {
+  
+      keys = Object.keys(snapshot.val());    
+  
+    });   
+
+    callback({
+        "keys":keys,
+    });
+
+  });
+}
+ //*******************************************************/
+
+ function firebaseGetValues(route,callback){
+
+    firebase.database().ref(route)
+    .once("value",function(snapshot){
+
+    var values = [];
+  
+
+    snapshot.forEach((childSnapshot) => {
+  
+        values.push(childSnapshot.val());   
+  
+    });   
+
+    callback({
+        "values":values,
+    });
+
+  });
+}  
+//*******************************************************/
+
+function firebaseActualDataPoints(id,actualDay){
+
+    firebase.database().ref("UsersChart/"+id +'/'+ actualDay).limitToLast(10)
+    .on("value",function(snapshot){ 
+  
+      var actualData = [];
+      var actualOxygen = [];
+      var actualPulse = [];
+      var actualHour = [];
+  
+      snapshot.forEach((childSnapshot) => {
+  
+        actualData.push(childSnapshot.val());
+  
+      })
+  
+      actualData.forEach((data => {
+  
+          actualOxygen.push(data.oxygen);
+          actualPulse.push(data.pulse);
+          actualHour.push(data.actualHour);
+      }))
+      chart(actualHour,actualOxygen,actualPulse,actualDay);
+    })
+  }
